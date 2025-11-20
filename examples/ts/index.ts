598956c95c583182450cb90b62514ebba6c27a42
Omer Cohen <git@omerc.net>
2025-11-20 19:24:05 +0700

    fix

--- examples/ts/index.ts
+++ examples/ts/index.ts
@@ -26,21 +26,6 @@ export const project = new descope.Project("pulumi-ts-test", {
       disabled: false,
     },
   },
-  authorization: {
-    permissions: [
-      {
-        name: "perm1",
-        description: "Permission 1",
-      },
-    ],
-    roles: [
-      {
-        key: "role1",
-        name: "role1",
-        permissions: ["perm1"],
-      },
-    ],
-  },
   connectors: {
     https: [{ name: "my_https", baseUrl: "https://example.com" }],
   },
